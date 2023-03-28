bb8a0c641f128a6327bf6a8be7896dc8d0e5bd59
Bruno Salmon <dev.salmonb@gmail.com>
2023-03-28 13:22:49 +0100

    Added InitMojo

---
+++ src/main/java/dev/webfx/mavenplugin/InitMojo.java
@@ -0,0 +1,54 @@
+package dev.webfx.mavenplugin;
+
+import dev.webfx.cli.mavenplugin.InitGoal;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.codehaus.plexus.components.interactivity.Prompter;
+import org.codehaus.plexus.components.interactivity.PrompterException;
+
+@Mojo(name = "init", aggregator = true) // aggregator = true because that goal doesn't need to be run on children
+public final class InitMojo extends AbstractMojo {
+	
+	/**
+	 * projectDirectory from the maven pom.xml file to
+	 * pass into the webfx-cli export object.
+	 */
+	@Parameter(property="projectDirectory", defaultValue="${basedir}")
+	private String projectDirectory;
+
+	/**
+	 * Stop executing build on error or ignore errors
+	 */
+	@Parameter(property="failOnError", defaultValue="true")
+	private boolean failOnError;
+
+	@Component
+	private Prompter prompter;
+
+	/**
+	 * Called when this goal is run
+	 */
+	@Override
+	public void execute() throws MojoFailureException {
+
+		getLog().debug("-------- export parameters --------");
+		getLog().debug("projectDirectory: " + projectDirectory);
+		getLog().debug("failOnError: " + failOnError);
+		getLog().debug("-----------------------------------");
+
+		try {
+			String artifact = prompter.prompt("Enter your artifact (expressed as groupId:artifactId:version)");
+			LoggerUtil.configureWebFXLoggerForMaven(getLog());
+			int result = InitGoal.init(projectDirectory, artifact);
+			if (failOnError && result != 0) {
+				throw new MojoFailureException("Failed to complete init, result=" + result);
+			}
+		} catch (PrompterException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+}
